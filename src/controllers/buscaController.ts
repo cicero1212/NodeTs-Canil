be5657e7b6a0eb5af8f42befdfc6388c584de210
cicero eduardo <ciceroedu1212@gmail.com>
2022-05-03 00:26:26 -0300

    Projeto Final

--- src/controllers/buscaController.ts
+++ src/controllers/buscaController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { Pet } from '../models/pet';
+import { createMenuObj } from '../helpers/createMenuObj';
 
 export const busca = (req: Request, res: Response)=> {
-    
+    let queryList: string = req.query.q as string
+
+    if(!queryList){
+        res.redirect('/')
+        return
+    }
+
+    let list = Pet.getFromName(queryList)
+
+    res.render('pages/page', {
+        menu: createMenuObj(''),
+        list,
+        queryList
+    })
 }
--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,57 @@
+//--------------------------------------------------//
 import { Request, Response } from "express";
-
+import { createMenuObj } from '../helpers/createMenuObj'
+import { Pet } from '../models/pet'
+//--------------------------------------------------//
 export const home = (req: Request, res: Response)=> {
-    
+    let list = Pet.getAll()
+
+    res.render('pages/page', {
+        menu: createMenuObj('all'),
+        banner: {
+            title: 'Todos os animais',
+            bg: 'allanimals.jpg'
+        },
+        list
+    })
 }
+//--------------------------------------------------//
 export const dogs = (req: Request, res: Response)=> {
-    
+    let list = Pet.getFromType('dog')
+
+    res.render('pages/page', {
+        menu: createMenuObj('dog'),
+        banner: {
+            title: 'Cachorros',
+            bg: 'banner_dog.jpg'
+        },
+        list
+    })
 }
+//--------------------------------------------------//
 export const cats = (req: Request, res: Response)=> {
-    
+    let list = Pet.getFromType('cat')
+
+    res.render('pages/page', {
+        menu: createMenuObj('cat'),
+        banner: {
+            title: 'Gatos',
+            bg: 'banner_cat.jpg'
+        },
+        list
+    })
 }
+//--------------------------------------------------//
 export const fishes = (req: Request, res: Response)=> {
-    
+    let list = Pet.getFromType('fish')
+
+    res.render('pages/page', {
+        menu: createMenuObj('fish'),
+        banner: {
+            title: 'Peixes',
+            bg: 'banner_fish.jpg'
+        },
+        list
+    })
 }
+//--------------------------------------------------//
